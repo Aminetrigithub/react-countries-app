b7bed1a40b6da8f48c7adadb39460d00d8b09d81
Amine Trigui <aminetrigui.github@gmail.com>
2024-03-08 05:34:37 +0100

    packages installation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootswatch/dist/sandstone/bootstrap.min.css";
+import "./App.css";
+import CountriesList from "./Components/Countries/CountriesList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="m-p">
+        <form className="d-flex ">
+          <input
+            className="form-control me-sm-2 "
+            type="search"
+            placeholder="Search"
+          />
+          <button className="btn btn-secondary my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+<CountriesList />
+
+    
     </div>
   );
 }
---
+++ src/Components/Countries/CountriesList.jsx
@@ -0,0 +1,21 @@
+
+function CountriesList() {
+
+  
+  return (
+    <div>  <div className="container">
+    <div class="card bg-light mb-3 m-p">
+      <div class="card-header">Header</div>
+      <div class="card-body">
+        <h4 class="card-title">Light card title</h4>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  </div></div>
+  )
+}
+
+export default CountriesList
